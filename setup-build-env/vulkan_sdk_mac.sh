4c4b81b6e00c249bf89b679e06eea0c3cb9eae80
assiduous <assiduous@diligentgraphics.com>
2025-03-13 15:38:18 -0700

    setup-build-env: bump default vulkan sdk version to 1.4.309.0 (v4)

--- setup-build-env/vulkan_sdk_mac.sh
+++ setup-build-env/vulkan_sdk_mac.sh
@@ -1,11 +1,18 @@
+set -e
+
 VULKAN_SDK_VER=$1
 
-export VK_SDK_DMG=vulkansdk-macos-$VULKAN_SDK_VER.dmg
-wget -O $VK_SDK_DMG https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VER/mac/$VK_SDK_DMG?Human=true && hdiutil attach $VK_SDK_DMG
-# https://vulkan.lunarg.com/doc/view/latest/mac/getting_started.html#user-content-install-and-uninstall-from-terminal
+# https://vulkan.lunarg.com/doc/view/latest/mac/getting_started.html#install-and-uninstall-from-terminal
+
+export VK_SDK_ZIP=vulkansdk-macos-$VULKAN_SDK_VER.zip
+wget -O "$VK_SDK_ZIP" https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VER/mac/$VK_SDK_ZIP?Human=true && unzip -q "$VK_SDK_ZIP"
 
 export VK_SDK_PATH=~/VulkanSDK
-sudo /Volumes/vulkansdk-macos-$VULKAN_SDK_VER/InstallVulkan.app/Contents/MacOS/InstallVulkan --root $VK_SDK_PATH --accept-licenses --default-answer --confirm-command install
+sudo ./InstallVulkan-$VULKAN_SDK_VER.app/Contents/MacOS/InstallVulkan-$VULKAN_SDK_VER --root $VK_SDK_PATH --accept-licenses --default-answer --confirm-command install
+
+rm "$VK_SDK_ZIP"
+rm -rf "InstallVulkan-$VULKAN_SDK_VER.app"
+
 if [ ! -f "$VK_SDK_PATH/macOS/lib/libvulkan.dylib" ]; then
     echo "Unable to find libvulkan.dylib in the SDK."
     exit 1
@@ -16,3 +23,5 @@ if [ ! -d "$VK_SDK_PATH/macOS/lib/MoltenVK.xcframework" ]; then
 fi
 
 echo "VULKAN_SDK=$VK_SDK_PATH" >> $GITHUB_ENV
+
+echo "Vulkan SDK $VULKAN_SDK_VER installed successfully!"
